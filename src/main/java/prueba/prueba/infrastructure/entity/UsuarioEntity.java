1dd727874220fcaec7f698964c3df3961d7ab71c
José Luis Domínguez Medina <jdominguez.yunius@gmail.com>
2026-01-31 02:14:25 -0600

    Modificación de la entidad de usuario

--- src/main/java/prueba/prueba/infrastructure/entity/UsuarioEntity.java
+++ src/main/java/prueba/prueba/infrastructure/entity/UsuarioEntity.java
@@ -22,10 +22,8 @@ public class UsuarioEntity implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String nombre;
 
-    @Column(nullable = false)
     private String apellidos;
 
     @Column(unique = true, nullable = false)
